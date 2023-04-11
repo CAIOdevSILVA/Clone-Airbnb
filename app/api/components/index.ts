a12a8f6df746e88b846de95b4abc333befcbea14
Caio Silva <caiossantosp5560@gmail.com>
2023-04-11 17:28:55 -0300

    add: Navbar Component

---
+++ app/api/components/index.ts
@@ -0,0 +1,11 @@
+//Navbar
+import Navbar from "./Navbar/Navbar";
+import Logo from "./Navbar/Logo"
+import Search from "./Navbar/Search";
+import UserMenu from "./Navbar/UserMenu";
+import MenuItem from "./Navbar/MenuItem";
+
+import Container from "./Container/Container";
+import Avatar from "./Avatar"
+
+export { Navbar, Container, Logo, Search, UserMenu, Avatar, MenuItem }
